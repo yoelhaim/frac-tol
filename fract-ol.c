408789c084f02221bfc5d1a0f8eca73dd0c654d4
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2022-03-13 20:47:31 +0100

    update : update code  norminette  and fixed errors display jolia

--- fract-ol.c
+++ fract-ol.c
@@ -6,7 +6,7 @@
 /*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/27 15:11:30 by yoelhaim          #+#    #+#             */
-/*   Updated: 2022/03/13 17:00:06 by yoelhaim         ###   ########.fr       */
+/*   Updated: 2022/03/13 20:43:52 by yoelhaim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,13 +31,23 @@ void	callmlx(t_fractol *data)
 	data->color = 0xff0801;
 }
 
-int	main(int ac, char **av)
+int	mouse_get_vals(int x, int y, t_fractol *data)
 {
-	t_fractol	data;
+	data->c_r = ft_map(x, 800, data->re_start - data->zoom, \
+	data->re_end + data->zoom);
+	data->c_i = ft_map(x, 800, data->im_start - data->zoom, \
+	data->im_end + data->zoom);
+	ft_draw(data);
+	return (0);
+}
 
+void	ft_fractol(t_fractol data)
+{
 	callmlx(&data);
 	mlx_hook(data.win, 2, 0, set_hook, &data);
 	mlx_mouse_hook (data.win, inmouse, &data);
 	ft_draw(&data);
+	if (data.julia)
+		mlx_hook(data.win, 6, 0, mouse_get_vals, &data);
 	mlx_loop(data.mlx);
 }
