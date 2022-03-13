133654d65ca3e4271460c8413baf8a942700bbc1
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2022-03-13 17:03:50 +0100

    main program

--- fract-ol.c
+++ fract-ol.c
@@ -5,10 +5,39 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/27 17:08:49 by yoelhaim          #+#    #+#             */
-/*   Updated: 2022/02/27 17:09:13 by yoelhaim         ###   ########.fr       */
+/*   Created: 2022/02/27 15:11:30 by yoelhaim          #+#    #+#             */
+/*   Updated: 2022/03/13 17:00:06 by yoelhaim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fract.h"
 
+void	callmlx(t_fractol *data)
+{
+	data->mlx = mlx_init();
+	data->win = mlx_new_window(data->mlx, 800, 800, "fract-ol");
+	data->img.img = mlx_new_image(data->mlx, 800, 800);
+	data->img.addr = mlx_get_data_addr(data->img.img, \
+	&data->img.bits_per_pixel, &data->img.line_length, &data->img.endian);
+	data->re_start = 0;
+	data->re_end = 0;
+	data->im_start = 0;
+	data->im_end = 0;
+	data->zoom = 2;
+	data->c_r = 0.285;
+	data->c_i = 0;
+	data->values = 100;
+	data->color_code = 0;
+	data->color = 0xff0801;
+}
+
+int	main(int ac, char **av)
+{
+	t_fractol	data;
+
+	callmlx(&data);
+	mlx_hook(data.win, 2, 0, set_hook, &data);
+	mlx_mouse_hook (data.win, inmouse, &data);
+	ft_draw(&data);
+	mlx_loop(data.mlx);
+}
