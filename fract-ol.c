68bacbbf64fdabbbc70358c3dafe2e437b1a46cc
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2022-03-14 15:21:17 +0100

    update in fractol

--- fract-ol.c
+++ fract-ol.c
@@ -6,7 +6,7 @@
 /*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/27 15:11:30 by yoelhaim          #+#    #+#             */
-/*   Updated: 2022/03/14 13:04:14 by yoelhaim         ###   ########.fr       */
+/*   Updated: 2022/03/14 15:19:53 by yoelhaim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 void	callmlx(t_fractol *data)
 {
 	data->mlx = mlx_init();
-	data->win = mlx_new_window(data->mlx, 800, 800, "fract-ol");
-	data->img.img = mlx_new_image(data->mlx, 800, 800);
+	data->win = mlx_new_window(data->mlx, WIDTH, HEIGHT, "fract-ol");
+	data->img.img = mlx_new_image(data->mlx, WIDTH, HEIGHT);
 	data->img.addr = mlx_get_data_addr(data->img.img, \
 	&data->img.bits_per_pixel, &data->img.line_length, &data->img.endian);
 	data->real_start = 0;
@@ -29,14 +29,18 @@ void	callmlx(t_fractol *data)
 	data->values = 100;
 	data->color_code = 0;
 	data->color = 0x1A40DC;
+	data->julia_set = 0;
 }
 
 int	mouse_get_vals(int x, int y, t_fractol *data)
 {
-	data->c_real = ft_map(x, 800, data->real_start - data->zoom, \
+	if (data->julia_set)
+	{
+		data->c_real = ft_map(x, WIDTH, data->real_start - data->zoom, \
 	data->real_end + data->zoom);
-	data->c_imagin = ft_map(y, 800, data->imagin_start - data->zoom, \
+	data->c_imagin = ft_map(y, HEIGHT, data->imagin_start - data->zoom, \
 	data->imagin_end + data->zoom);
+	}
 	ft_draw(data);
 	return (0);
 }
@@ -48,6 +52,9 @@ void	ft_fractol(t_fractol data)
 	mlx_mouse_hook (data.win, inmouse, &data);
 	ft_draw(&data);
 	if (data.julia)
+	{
+		data.julia_set = 1;
 		mlx_hook(data.win, 6, 0, mouse_get_vals, &data);
+	}
 	mlx_loop(data.mlx);
 }
